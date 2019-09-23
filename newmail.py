c2e5953db2da57435d7dd26ad2a7ba67715ab134
Hugo Hörnquist <hugo@hornquist.se>
2019-09-24 00:08:13 +0200

    Port program to python, works much better.

---
+++ newmail.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+
+mailroot = "/home/hugo/mail"
+
+def p(path):
+    """prettify path"""
+    return path[len(mailroot) + 1:]
+
+def c(count):
+    if count < 10:
+        color = 2 # green
+    elif count < 100:
+        color = 3 # yellow
+    else:
+        color = 1 # red
+
+    return f"\x1b[1;3{color}m"
+
+def plural(count):
+    return "es" if count != 0 else ""
+
+if __name__ == "__main__":
+    gen = os.walk(mailroot)
+    accounts = []
+    for (path, dirs, files) in gen:
+        if dirs == ["tmp", "cur", "new"]:
+            tmp = next(gen)
+            cur = next(gen)
+            (_, _, files) = next(gen)
+            count = len(files)
+            if count != 0:
+                accounts.append((path, count))
+
+    for (path, count) in sorted(accounts, key=lambda p: p[0]):
+        print(f"{c(count)}{count:>6}\x1b[m {p(path)}")
+
+    l = len(accounts)
+    print(f"\nNew mail in {c(l)}{l}\x1b[m mailbox{plural(l)}.")
